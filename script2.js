46b528059b79f60cdaec17afbd05bbfa64f0fc40
Akmal Aiman Muhammad Ali <akmal.aaiman16@gmail.com>
2024-07-21 16:36:44 +0800

    New template files

---
+++ script2.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".multiple-text", {
+    strings: [
+        "Full Stack Developer",
+        "Software Engineer"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+});
