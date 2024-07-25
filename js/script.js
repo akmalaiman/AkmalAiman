9db3e90c86584d710a78e21e83454f6225d94b00
Akmal Aiman <akmal.aaiman16@gmail.com>
2024-07-25 22:50:38 +0800

    Add styling for header & navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 //Display current year for footer
 const currentYear = new Date().getFullYear();
 document.getElementById("footer-year").innerHTML = currentYear;
+
+//Typing Animation
+var typed = new Typed(".multiple-text", {
+        strings: [
+                "Full Stack Developer",
+                "Software Engineer"
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        backDelay: 1000,
+        loop: true
+});
