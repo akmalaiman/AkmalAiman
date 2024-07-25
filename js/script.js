e00278ab6bff73881a5a4ace6ca8ac9bef9a650c
Akmal Aiman <akmal.aaiman16@gmail.com>
2024-07-25 23:24:37 +0800

    Add styling for home section

--- js/script.js
+++ js/script.js
@@ -13,3 +13,40 @@ var typed = new Typed(".multiple-text", {
         backDelay: 1000,
         loop: true
 });
+
+//Toggle Icon Navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+        menuIcon.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+};
+
+//Scroll Section Active Link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+        sections.forEach(sec => {
+                let top = window.scrollY;
+                let offset = sec.offsetTop - 150;
+                let height = sec.offsetHeight;
+                let id = sec.getAttribute('id');
+
+                if (top >= offset && top < offset + height) {
+                        navLinks.forEach(link => {
+                                link.classList.remove('active');
+                                document.querySelector('header nav a[href*='+id+']').classList.add('active')
+                        });
+                }
+        });
+
+        /*Sticky Navbar*/
+        let header = document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY > 100);
+
+        /*Remove toggle icon and navbar when the navbar link is click*/
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+};
