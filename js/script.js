04f71951b351a0a6a32dcf18e62ad4e1bd4353a8
Akmal Aiman <akmal.aaiman16@gmail.com>
2024-08-04 18:23:39 +0800

    Add typing animation and header active class

--- js/script.js
+++ js/script.js
@@ -38,6 +38,7 @@ window.onscroll = () => {
                         navLinks.forEach(link => {
                                 link.classList.remove('active');
                                 document.querySelector('header nav a[href*='+id+']').classList.add('active')
+                                console.log(id);
                         });
                 }
         });
@@ -50,3 +51,16 @@ window.onscroll = () => {
         menuIcon.classList.remove('bx-x');
         navbar.classList.remove('active');
 };
+
+//Scroll Reveal
+ScrollReveal({
+        reset: true,
+        distance: '80px',
+        duration: 2000,
+        delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .portfolio-box, .contact-form, .journey-box, .journey-content', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img, .journey-column .title', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content,', { origin: 'right' });
